41f5a2eed6d4925a9e133578fe183554edac892a
Jeremy Low <jrmylow@gmail.com>
2019-12-28 13:47:57 +1100

    shifting of structure, implemented calc_coord_Cost

--- prototype/dsm_helper_classes.py
+++ prototype/dsm_helper_classes.py
@@ -17,6 +17,29 @@ class DsmMatrix(object):
         """Allows use of python built-in len() function"""
         return len(self.act_labels)
 
+    def Reorder_DSM_by_Cluster(self, dsm_cluster):
+        """Returns a new dsm_matrix object expanding out the elements that are in multiple clusters. Also recalculates the size of this matrix"""
+
+        # return DSM_matrix(new_mat, new_labels)
+        cl_mat = dsm_cluster.cluster_mat
+        ds_mat = self.mat
+
+        num_ele = cl_mat[cl_mat > 0].size
+        temp_mat = np.zeros((num_ele, len(self)))
+        new_dsm_mat = DsmMatrix(np.zeros((num_ele,num_ele)), np.zeros(num_ele))
+
+        ii = 0
+        for cluster in cl_mat:
+            for ind, flag in enumerate(cluster):
+                if flag:
+                    temp_mat[ii,:] = ds_mat[ind,:]
+                    new_dsm_mat.act_labels[ii] = ind
+                    ii += 1
+
+        for ind, col in enumerate(new_dsm_mat.act_labels):
+            new_dsm_mat.mat[:,ind] = temp_mat[:,col]
+
+        return new_dsm_mat
 
 class DsmCluster(object):
     """
@@ -39,35 +62,40 @@ class DsmCluster(object):
     def calc_coord_cost(self, dsm_matrix, cluster_parameters):
         """Calculates and updates the coordination cost of the current matrix"""
         coord_cost_list = []
-        new_dsm_mat = self.Reorder_DSM_by_Cluster(dsm_matrix, self)
-        new_clu_mat = np.zeros((len(new_dsm_mat), len(new_dsm_mat)))
+
+        # Gets a new DSM matrix with duplicate rows depending on clustering
+        new_dsm_mat = dsm_matrix.Reorder_DSM_by_Cluster(self)
+
+        # Builds the new cluster matrix according to the dsm structure
+        new_clu_mat = np.zeros((len(new_dsm_mat), len(self.cluster_size)))
+        start = 0
+        for ind, row in enumerate(self.cluster_mat):
+            fin = start + row[row != 0].size
+            new_clu_mat[ind, start:fin] = 1
+            start = fin
+
+        for ii in range(len(new_dsm_mat)):
+            coord_cost_list.append(0)
+            for jj in range(ii + 1, len(new_dsm_mat)):
+                if (new_dsm_mat[ii, jj] > 0) or (new_dsm_mat[jj, ii] > 0):
+                    cost_tot = 0
+
+                    for cluster in new_clu_mat:
+                        if (cluster[ii]) and (cluster[jj]):
+                            cost_tot += (new_dsm_mat[ii,jj] \
+                                        + new_dsm_mat[jj,ii]) \
+                                        * (cluster[cluster != 0].size \
+                                        ** cluster_parameters.pow_cc)
+
+                    if cost_tot > 0:
+                        coord_cost_list[ii] += cost_tot
+                    else:
+                        coord_cost_list[ii] += (new_dsm_mat[ii,jj] \
+                                                + new_dsm_mat[jj,ii]) \
+                                                * (len(new_dsm_mat) \
+                                                ** cluster_parameters.pow_cc)
+
         self.total_coord_cost = sum(coord_cost_list)
-        raise NotImplementedError
-
-    @staticmethod
-    def Reorder_DSM_by_Cluster(dsm_matrix, dsm_cluster):
-        """Returns a new dsm_matrix object expanding out the elements that are in multiple clusters. Also recalculates the size of this matrix"""
-
-        # return DSM_matrix(new_mat, new_labels)
-        cl_mat = dsm_cluster.cluster_mat
-        ds_mat = dsm_matrix.mat
-
-        num_ele = cl_mat[cl_mat > 0].size
-        temp_mat = np.zeros((num_ele, len(dsm_matrix)))
-        new_dsm_mat = DsmMatrix(np.zeros((num_ele,num_ele)), np.zeros(num_ele))
-
-        ii = 0
-        for cluster in cl_mat:
-            for flag, ind in enumerate(cluster):
-                if flag:
-                    temp_mat[ii,:] = ds_mat[ind,:]
-                    new_dsm_mat.act_labels[ii] = ind
-                    ii += 1
-
-        for ind, col in enumerate(new_dsm_mat.act_labels):
-            new_dsm_mat.mat[:,ind] = temp_mat[:,col]
-
-        return new_dsm_mat
 
     def update_cluster_mat(self, element, cluster_list):
         """Updates the matrix to reflect a new bid in cluster_list"""
