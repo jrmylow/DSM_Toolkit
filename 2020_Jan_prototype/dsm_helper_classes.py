586e14bf5e5b8160e6bcc735aa5d2e46efccc7d1
Jeremy Low <jrmylow@gmail.com>
2020-01-11 10:57:11 +1100

    successfully implemented reorder_by_cluster

--- 2020_Jan_prototype/dsm_helper_classes.py
+++ 2020_Jan_prototype/dsm_helper_classes.py
@@ -1,3 +1,12 @@
+""" Contains helper classes for containing the relevant DSM data
+    1. ClusterParameters is for defining the behaviour of a specific optimisation algorithm
+    2. DSMMatrix
+    3. ClusterMatrix
+"""
+from collections import Counter
+import copy
+import numpy as np
+
 # ----- Cluster Parameters -----------------------------------------------------
 class ClusterParameters(object):
     """ Constants that define the behaviour of the optimisation algorithm
@@ -35,3 +44,95 @@ def set_default_cluster_parameters(max_size):
         max_repeat = 10
     )
 # ------------------------------------------------------------------------------
+
+class DSMMatrix(object):
+    """ Lists activities in act_labels and dependencies in mat (2-d matrix)
+    """
+    def __init__(self, mat, act_labels):
+        """ Default constructor is to accept a matrix and list of labels. Checks size of matrix and labels for correctness
+        """
+        assert len(mat.shape) == 2, "Input matrix must be two-dimensional"
+        assert mat.shape[0] == mat.shape[1], "Input matrix must be square"
+        assert len(act_labels) == mat.shape[0], "Labels must match matrix"
+
+        self.mat = mat
+        self.act_labels = act_labels
+    
+    def __len__(self):
+        """ Built in len() method gives characteristic dimension of container
+        """
+        return len(self.act_labels)
+
+    @classmethod
+    def from_size(cls, size):
+        """ Alternate constructor for DSM matrix creating a blank matrix of given size and act_labels with all "None" values
+        """
+        mat = np.zeros([size, size])
+        act_labels = [None for _ in range(size)]
+        return cls(mat, act_labels)
+
+    @staticmethod
+    def reorder_by_cluster(dsm_matrix, cluster_matrix):
+        """ Returns a new dsm_matrix object expanding out the elements in multiple clusters, also recalculating the size of this matrix
+        """
+        cl_mat = cluster_matrix.mat
+        ds_mat = dsm_matrix.mat
+
+        num_ele = cl_mat[cl_mat > 0].size
+        temp_mat = np.zeros([num_ele, len(dsm_matrix)])
+        new_ds_mat = DSMMatrix.from_size(num_ele)
+
+        ii = 0
+        for cluster in cl_mat:
+            for ind, flag in enumerate(cluster):
+                if flag:
+                    temp_mat[ii,:] = ds_mat[ind,:]
+                    new_ds_mat.act_labels[ii] = ind
+                    ii += 1
+
+        for ind, col in enumerate(new_ds_mat.act_labels):
+            new_ds_mat.mat[:,ind] = temp_mat[:,col]
+            new_ds_mat.act_labels[ind] = dsm_matrix.act_labels[col]
+
+        return new_ds_mat
+
+class ClusterMatrix(object):
+    def __init__(self, n_clus):
+        """ Default constructor is to create a symmetric matrix and vector to track the cluster sizes
+        """
+        self.mat = np.zeros((n_clus, n_clus))
+        self._num_activities = n_clus
+        self.update_cluster_size()
+        self.total_coord_cost = 0
+
+    @classmethod
+    def from_mat(cls, mat):
+        """ Alternate constructor from a given input matrix
+        """
+        num_act = mat.shape[1]
+        cluster_mat = ClusterMatrix(num_act)
+        cluster_mat.mat = mat
+        cluster_mat.update_cluster_size()
+
+        return cluster_mat
+
+    @property
+    def num_clusters(self):
+        return len(self.cluster_size)
+
+    @property
+    def num_activities(self):
+        return self._num_activities
+
+    def update_cluster_size(self):
+        """ Updates the cluster_size array to reflect changes in mat
+        """
+        self.cluster_size = np.array([sum(row) for row in self.mat])
+
+    def update_mat(self, element, cluster_list):
+        """ Updates the matrix to reflect a new bid in input cluster_list
+        """
+        assert len(cluster_list) == self.num_activities, "Number of elements in cluster_list must match the number of activities in this cluster matrix"
+        self.mat[:,element] = \
+            np.logical_or(self.mat[:,element], cluster_list)
+
--- 2020_Jan_prototype/test_suite.py
+++ 2020_Jan_prototype/test_suite.py
@@ -6,6 +6,21 @@ from clustering_functions import *
 from dsm_helper_classes import *
 from plotting_functions import *
 
+# Test case for reorder_by_cluster
+if True:    
+    d_mat = np.identity(3)
+    d_list = ["1", "2", "3"]
+    d = DSMMatrix(d_mat, d_list)
+    print(d.mat)
+
+    c_mat = np.array([[1, 1, 1], [0, 1, 1]])
+    c = ClusterMatrix.from_mat(c_mat)
+    print(c.mat)
+
+    new_d_mat = DSMMatrix.reorder_by_cluster(d, c)
+    print(new_d_mat.mat)
+    print(new_d_mat.act_labels)
+
 # Tests for dsm_helper_classes
 class ClusterParametersTestCase(unittest.TestCase):
     raise NotImplementedError
